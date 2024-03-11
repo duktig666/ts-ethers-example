afb5f134ec975545786a1671bab2d88c2c51f663
RenShiWei <ren_shi_wei@qq.com>
2024-03-11 11:44:53 +0800

    seaport

--- src/contracts/contract.ts
+++ src/contracts/contract.ts
@@ -1,10 +1,11 @@
-
 import {ethers} from "ethers";
 import {config} from "../config/config";
+import {Signer} from 'ethers'
 
 const executionLayerAddr = config.executionLayerAddr;
 const provider = new ethers.providers.JsonRpcProvider(executionLayerAddr);
 const privateKey = config.privateKey;
 const wallet = new ethers.Wallet(privateKey, provider);
+const signer: Signer = wallet as Signer;
 
-export {provider,wallet}
+export {provider, wallet, signer}
